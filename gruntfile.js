a9fff0fa09d88b02265be235385e43e7e7def5ed
David Reichert <wildcat1221@gmail.com>
2014-05-03 18:51:48 -0500

    First Commit

---
+++ gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            js: {
+                files: ['gruntfile.js', 'server.js', 'server/**/*.js', 'public/js/**'],
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: ['public/views/**', 'server/views/**'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        jshint: {
+            all: {
+                src: ['gruntfile.js', 'server.js', 'server/**/*.js', 'public/js/**'],
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'server.js',
+                options: {
+                    args: [],
+                    ignore: ['public/**'],
+                    ext: 'js,html',
+                    nodeArgs: ['--debug'],
+                    delayTime: 1,
+                    env: {
+                        PORT: 8080
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+    });
+
+    //Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    //Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
+
+    //Default task(s).
+    if (process.env.NODE_ENV === 'production') {
+        grunt.registerTask('default', ['jshint', 'concurrent']);
+    } else {
+        grunt.registerTask('default', ['jshint', 'concurrent']);
+    }
+
+    //Test task.
+    //grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+};
